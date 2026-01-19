968052fdaf4fb11e03f432877a82cff4253361cf
Satyamit86 <pandeysatyam9833@gmail.com>
2026-01-19 17:12:22 +0530

    utils

---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,8 @@
+class ApiResonse {
+    constructor(status, data, message="success") {
+        this.status = status;
+        this.data = data;
+        this.message = message;
+        this.success = status >= 200 && status < 300;
+}
+}
